194748ad1c187d047ccead11e0b0c36cbbdbe26c
Jason Kusnier <jkusnier@gmail.com>
2014-10-31 21:58:08 -0400

    added refresh button

--- app/controllers/DeviceCtrl.js
+++ app/controllers/DeviceCtrl.js
@@ -1,8 +1,19 @@
 'use strict';
 
-function DeviceCtrl($$getEnvironment) {
+function DeviceCtrl($route, GreenhouseService, $$getEnvironment) {
     var vm = this;
     vm.environment = $$getEnvironment.data;
+
+    vm.refreshData = refreshData;
+
+    function refreshData(id) {
+        GreenhouseService.getEnvironment(id)
+            .then(function (resp) {
+                vm.environment = resp.data;
+            }, function (err) {
+                console.error(err);
+            });
+    }
 }
 
 DeviceCtrl.resolve = {
